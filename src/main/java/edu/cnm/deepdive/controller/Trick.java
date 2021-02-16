dd4c327d61baf1f3e516011efeebb31279feadf3
Nick Bennett <nick@nickbenn.com>
2021-02-16 14:02:05 -0700

    Implemented comparator as anonymous class.

--- src/main/java/edu/cnm/deepdive/controller/Trick.java
+++ src/main/java/edu/cnm/deepdive/controller/Trick.java
@@ -60,8 +60,7 @@ public class Trick {
   }
 
   private void sort() {
-    class CardColorComparator implements Comparator<Card> {
-
+    Comparator<Card> comparator = new Comparator<Card>() {
       @Override
       public int compare(Card card1, Card card2) {
         int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
@@ -73,9 +72,7 @@ public class Trick {
         }
         return comparison;
       }
-
-    }
-    Comparator<Card> comparator = new CardColorComparator();
+    };
     redPile.sort(comparator);
     blackPile.sort(comparator);
   }
